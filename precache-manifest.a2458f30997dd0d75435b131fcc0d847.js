f9e3ca98bb72fa4c1fe998d51433491b8a3e5f79
SoninVI <v.sonin1986@gmail.com>
2020-03-25 17:38:22 +0200

    Updates

---
+++ precache-manifest.a2458f30997dd0d75435b131fcc0d847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e5faac4636e6eb6c29a50f89cb5dbf",
+    "url": "/ReactDZ24.03/index.html"
+  },
+  {
+    "revision": "4953570621b434b569e7",
+    "url": "/ReactDZ24.03/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "3d15554cdf14b965a851",
+    "url": "/ReactDZ24.03/static/css/main.f32bd8a5.chunk.css"
+  },
+  {
+    "revision": "4953570621b434b569e7",
+    "url": "/ReactDZ24.03/static/js/2.a3922de1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactDZ24.03/static/js/2.a3922de1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d15554cdf14b965a851",
+    "url": "/ReactDZ24.03/static/js/main.93bfd306.chunk.js"
+  },
+  {
+    "revision": "c9e4d11cf1b17b059328",
+    "url": "/ReactDZ24.03/static/js/runtime-main.e05f2f51.js"
+  }
+]);
